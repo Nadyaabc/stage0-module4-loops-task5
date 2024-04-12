bc2401b4cdf18317520fef0dd06fe0f0ac2fc850
Nadejda <nadyash295923@gmail.com>
2024-04-12 23:07:43 +0300

    done hourglass

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,43 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        if (height!=0 && height %2 !=0)
+        {
+            for (int i = 0; i < height/2+1;i++)
+            {
+                for(int j = 0; j < i; j++) System.out.print(" ");
+                System.out.print(8);
+                for(int j = 0; j <height-(i+1)*2; j++ ) System.out.print("8");
+                if(i!=height/2 ) System.out.print(8);
+                System.out.println("");
 
+            }
+            for (int i = height/2; i > 0;i--)
+            {
+                for(int j = height/2-1; j >(height/2- i); j--) System.out.print(" ");
+                System.out.print(8);
+                for(int j = 0; j <height - 2*i; j++ ) System.out.print("8");
+                System.out.print(8);
+                System.out.println("");
+            }
+        }
+        else if (height !=0 && height%2==0)
+        {
+            for (int i = 0; i < height/2;i++)
+            {
+                for(int j = 0; j < i; j++) System.out.print(" ");
+                for(int j = 0; j < height-(i)*2; j++ ) System.out.print("8");
+                System.out.println("");
+            }
+
+            for (int i = height/2-1; i >= 0;i--)
+            {
+
+                for(int j = height/2; j >(height/2- i); j--) System.out.print(" ");
+                for(int j = 0; j <height-2*i ; j++ ) System.out.print("8");
+                System.out.println("");
+            }
+        }
     }
+
 }
