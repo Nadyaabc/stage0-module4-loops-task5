afc099c50630a7bf82ebfceeac8764e257433621
Nadejda <nadyash295923@gmail.com>
2024-04-12 22:13:59 +0300

    upd cross

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-        for (int i = 0; i < sideLength/2+1;i++)
+       /*for (int i = 0; i < sideLength/2+1;i++)
         {
             for(int j = 0; j < i; j++) System.out.print(" ");
             System.out.print(8);
@@ -20,6 +20,25 @@ public class Cross {
             System.out.print(8);
             //   for (int j = 0; j < i; j++) System.out.print("4");
             System.out.println("");
+        }*/
+        if (sideLength != 0) {
+            for (int i = 0; i < sideLength/2; i++)
+            {
+                for (int j = 0; j < sideLength/2; j++) System.out.print(" ");
+                System.out.print(8);
+                System.out.println("");
+            }
+            for (int i = 0; i < sideLength;i++ ) System.out.print(8);
+            System.out.println();
+            for (int i = 0; i < sideLength/2; i++)
+            {
+                for (int j = 0; j < sideLength/2; j++) System.out.print(" ");
+                System.out.print(8);
+                System.out.println("");
+            }
         }
+
+
+
     }
 }
