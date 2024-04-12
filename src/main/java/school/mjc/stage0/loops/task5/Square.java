ce06b6fd12e75a503a98e646fb0968368bc6ff2a
Nadejda <nadyash295923@gmail.com>
2024-04-12 18:54:17 +0300

    done square

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-
+        for (int i = 0; i < sideLength;i++) {
+            if (i == 0 || i == (sideLength - 1)) {
+                for (int j = 0; j < sideLength; j++) {
+                    System.out.print(8);
+                }
+                System.out.println("");
+            } else {
+                System.out.print(8);
+                for (int k = 1; k < sideLength - 1; k++) System.out.print(" ");
+                System.out.println(8);
+            }
+        }
     }
 }
