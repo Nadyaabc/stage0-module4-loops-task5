c943cbee5d8bf3f9cfb9b5b85774e1eec416d8bf
Nadejda <nadyash295923@gmail.com>
2024-04-12 22:19:36 +0300

    done Triangle

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
+        if (cathetusLength !=0) {
 
+            for (int i = 0; i < cathetusLength; i++) {
+                for (int j = 0; j<=i; j++)
+                    System.out.print(8);
+                System.out.println();
+            }
+        }
     }
 }
